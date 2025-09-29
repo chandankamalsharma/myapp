d0382c8fbef248473bae335cfbd0686a971b9332
chandankamalsharma <51348596+chandankamalsharma@users.noreply.github.com>
2025-09-29 17:04:57 +0400

    Update route.ts

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -24,7 +24,7 @@ export async function POST(request: Request) {
     // Forward the request to Pardot
     console.log("Sending data to Pardot...");
     const pardotResponse = await fetch(
-      "https://go.mic.life/l/1085292/2025-01-30/8q2gt6",
+      "https://go.ichras.com/l/1085292/2025-01-30/8q2dgs",
       {
         method: "POST",
         headers: {
@@ -122,4 +122,5 @@ export async function OPTIONS() {
     },
   );
 }
+
 
