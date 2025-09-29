174719a7426f9cd6470490e679c2dd9634144834
chandankamalsharma <51348596+chandankamalsharma@users.noreply.github.com>
2025-09-29 19:12:06 +0400

    Update page.tsx

--- src/app/(main)/(blue)/contactus/page.tsx
+++ src/app/(main)/(blue)/contactus/page.tsx
@@ -22,26 +22,5 @@ const ContactForm = () => {
     </div>
   );
 };
-import { useEffect } from "react";
 
-export default function CalendlyEmbed() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://assets.calendly.com/assets/external/widget.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  return (
-    <div
-      className="calendly-inline-widget"
-      data-url="https://calendly.com/ichras_com/exploratory-meeting"
-      style={{ minWidth: "320px", height: "700px" }}
-    />
-  );
-}
 export default ContactForm;
